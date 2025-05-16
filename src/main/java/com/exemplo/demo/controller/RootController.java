0bb471d7f7137b3dd1763f3ae993efd9413a912e
caiolucasxz55 <caiolucasxz55@gmail.com>
2025-05-16 17:01:41 -0300

    teste deploy java

---
+++ src/main/java/com/exemplo/demo/controller/RootController.java
@@ -0,0 +1,14 @@
+package com.exemplo.demo.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String root() {
+        return "Seu Java está funcionando!";
+    }
+}
