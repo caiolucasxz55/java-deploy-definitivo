cc83b5ceeddca317a734f40216df98493577ed13
caiolucasxz55 <caiolucasxz55@gmail.com>
2025-05-22 08:10:10 -0300

    implementando front no back

--- src/main/java/com/exemplo/demo/controller/UserController.java
+++ src/main/java/com/exemplo/demo/controller/UserController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "https://challenge-ccr-definitivo.vercel.app")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
